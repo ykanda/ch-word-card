60617b8233f271f3cae3781125330424bcc9edd5
Yasuhiro KANDA <yasuhiro.kanda@gmail.com>
2015-05-16 23:09:54 +0900

    Add ad

--- src/vue/page/top.vue
+++ src/vue/page/top.vue
@@ -1,8 +1,22 @@
 <template>
-  <div class="jumbotron" class='text-center'>
-    <h1>日文・中文<br>単語練習帳</h1>
-    <p class="text-center">...</p>
-    <p class="text-center"><a  href="/card" class="btn btn-primary btn-lg" href="#" role="button">単語帳を開く</a></p>
+  <div class="container">
+    <div class="jumbotron" class='text-center'>
+      <h1 class="text-center">日文・中文<br>単語練習帳</h1>
+      <p class="text-center">...</p>
+      <p class="text-center"><a  href="/card" class="btn btn-primary btn-lg" href="#" role="button">単語帳を開く</a></p>
+    </div>
+  </div>
+  <div class="container">
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- 中国語単語帳 -->
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-9507195460927673"
+         data-ad-slot="6188667983"
+         data-ad-format="auto"></ins>
+    <script>
+      (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
   </div>
 </template>
 
